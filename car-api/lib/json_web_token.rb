f77ea5ea69ac0dc668f56e7903fdbacbeb29bb5e
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-14 01:05:40 -0300

    Setup tokens controller

--- car-api/lib/json_web_token.rb
+++ car-api/lib/json_web_token.rb
@@ -1,9 +1,9 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
 
-  def self.encode(payload, exp = 24.hour.from_now)
+  def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload.SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
