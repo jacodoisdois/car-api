97095fda807a6afb080c67c2083a0708245f102e
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-14 01:27:44 -0300

    Restrict actions for unauthorized users

--- car-api/app/controllers/api/v1/users_controller.rb
+++ car-api/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 # User Controller
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   def show
     render json: User.find(params[:id])
@@ -37,5 +38,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :name)
+  end
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
   end
 end
