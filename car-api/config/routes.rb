62c895a73c43e6a6dfa672f597cae9505617d5a9
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-21 19:28:59 -0300

    update customer routes delete wrong routes

--- car-api/config/routes.rb
+++ car-api/config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'orders/create'
-      get 'orders/index'
-      get 'orders/show'
-      get 'orders/destroy'
-      get 'orders/update'
-    end
-  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
-      resources :customers, only: %i[show create update destroy]
+      resources :customers, only: %i[show create update destroy index]
       resources :tokens, only: [:create]
       resources :products, only: %i[create update destroy show index]
       resources :orders, only: %i[create index show update destroy]
