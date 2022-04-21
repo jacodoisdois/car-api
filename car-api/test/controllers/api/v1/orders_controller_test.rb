11baf7beddfe85e6bcd744289e11f8e100368024
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-21 06:42:50 -0300

    order_test

--- car-api/test/controllers/api/v1/orders_controller_test.rb
+++ car-api/test/controllers/api/v1/orders_controller_test.rb
@@ -1,28 +1,76 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_v1_orders_create_url
+  setup do
+    @order = orders(:one)
+    @user = users(:one)
+    @customer = customers(:one)
+
+    @auth = JsonWebToken.encode(user_id: @user.id)
+
+    @order_params = {
+      customer_id: @customer.id,
+      order: {
+        product_ids_and_quantities: [
+          { product_id: products(:one).id, quantity: 10 },
+          { product_id: products(:two).id, quantity: 10 }
+        ]
+      }
+    }
+  end
+
+  test 'should create order' do
+    assert_difference('Order.count', 1) do
+      assert_difference('OrderProduct.count', 2) do
+        post api_v1_orders_url,
+             params: @order_params,
+             headers: { Authorization: @auth },
+             as: :json
+      end
+      assert_response :success
+    end
+  end
+
+  test 'should unauthorized for unlogged' do
+    get api_v1_orders_url, as: :json
+    assert_response :unauthorized
+  end
+
+  test 'should index orders' do
+    get api_v1_orders_url,
+        headers: { Authorization: @auth },
+        as: :json
+
     assert_response :success
   end
 
-  test "should get index" do
-    get api_v1_orders_index_url
+  test 'should show order' do
+    get api_v1_orders_url(id: @order.id),
+        headers: { Authorization: @auth },
+        as: :json
+
     assert_response :success
   end
 
-  test "should get show" do
-    get api_v1_orders_show_url
-    assert_response :success
+  test 'should destroy order' do
+    assert_difference('Order.count', -1) do
+      delete api_v1_order_url(id: @order.id),
+             headers: { Authorization: @auth },
+             as: :json
+    end
+    assert_response :no_content
   end
 
-  test "should get destroy" do
-    get api_v1_orders_destroy_url
-    assert_response :success
-  end
+  test 'should update order' do
+    patch api_v1_order_url(@order),
+          params: {
+            order: {
+              order_products: []
+            }
+          },
+          headers: { Authorization: @auth },
+          as: :json
 
-  test "should get update" do
-    get api_v1_orders_update_url
     assert_response :success
   end
 end
