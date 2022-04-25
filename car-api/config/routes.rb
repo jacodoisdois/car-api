6b48738977215d68c43e7ad954223a169673cfbe
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-24 23:49:15 -0300

    update routes

--- car-api/config/routes.rb
+++ car-api/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create update destroy]
-      resources :customers, only: %i[show create update destroy index]
+      resources :users, only: %i[show create update destroy index]
+      resources :customers, only: %i[show index create update destroy]
       resources :tokens, only: [:create]
       resources :products, only: %i[create update destroy show index]
       resources :orders, only: %i[create index show update destroy]
