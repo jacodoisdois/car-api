536628743d885195e9a5aa619c17d672432b31d9
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-25 07:23:52 -0300

    validate user password

--- car-api/app/models/user.rb
+++ car-api/app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
                     presence: true
   validates :nickname, uniqueness: true
   validates :password_digest, presence: true, allow_blank: false
+  validates :password, presence: true, allow_blank: false,
+                       format: { with: /(?=.*[!@#$%^&*])(?=.*[0-9])/,
+                                 message: 'The password must have at least one special character and numbers' },
+                       length: { minimum: 8, maximum: 100 }
+
   has_secure_password
 
   scope :filter_by_name, lambda { |keyword|
-                           where("(lower(description) LIKE '%#{keyword.downcase}%' or lower(title) LIKE '%#{keyword.downcase}%') ")
+                           where("(lower(description) LIKE '%#{keyword.downcase}%' or
+                           lower(title) LIKE '%#{keyword.downcase}%') ")
                          }
 
   scope :recent, lambda { |order|
