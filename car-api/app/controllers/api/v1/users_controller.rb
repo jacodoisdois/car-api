9c544ba94c03a94946878b577072f574003f2f05
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-24 23:48:21 -0300

    User serializer

--- car-api/app/controllers/api/v1/users_controller.rb
+++ car-api/app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,40 @@
 # User Controller
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  before_action :check_owner, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
+  before_action :check_login, only: %i[index update destroy show]
+
+  include Paginable
+
+  def index
+    @users = User.page(current_page)
+                 .per(per_page)
+                 .search(params)
+
+    options = get_links_serializer_options('api_v1_users_path', @users)
+
+    render json: UserSerializer.new(@users, options).serializable_hash
+  end
 
   def show
-    render json: User.find(params[:id])
+    render json: UserSerializer.new(@user).serializable_hash
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors }, status: :unprocessable_entity
     end
   end
 
   def update
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: UserSerializer.new(@user).serializable_hash, status: :ok
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors }, status: :unprocessable_entity
     end
   end
 
@@ -37,7 +50,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :email, :password, :name, :nickname)
+    params.require(:user).permit(:id, :email, :nickname, :password, :name)
   end
 
   def check_owner
--- car-api/app/models/user.rb
+++ car-api/app/models/user.rb
@@ -4,4 +4,25 @@ class User < ApplicationRecord
   validates :nickname, uniqueness: true
   validates :password_digest, presence: true, allow_blank: false
   has_secure_password
+
+  scope :filter_by_name, lambda { |keyword|
+                           where("(lower(description) LIKE '%#{keyword.downcase}%' or lower(title) LIKE '%#{keyword.downcase}%') ")
+                         }
+
+  scope :recent, lambda { |order|
+    order == 'true' ? order(created_at: :desc) : order(created_at: :asc)
+  }
+
+  def self.search(params = {})
+    users = if params[:user_ids].present?
+              User.find(params[:user_ids])
+            else
+              User.all
+            end
+
+    users = users.filter_by_name(params[:keyword]) if params[:keyword]
+    users = users.recent(params[:recent]) if params[:recent].present?
+
+    users
+  end
 end
