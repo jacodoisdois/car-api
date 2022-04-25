cdb1eee75411dba5f254f276b49ae20ed6c6a191
Jaco Magalhães <jacomagalhaes@gmail.com>
2022-04-24 23:49:56 -0300

    update schema

--- car-api/db/schema.rb
+++ car-api/db/schema.rb
@@ -10,121 +10,120 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_231142) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "address"
-    t.string "address_line_two"
-    t.integer "number"
-    t.string "district"
-    t.string "city"
-    t.string "state"
-    t.string "zip_code"
-    t.boolean "main_address"
-    t.integer "customer_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_addresses_on_customer_id", unique: true
+ActiveRecord::Schema.define(version: 20_220_327_231_142) do
+  create_table 'addresses', force: :cascade do |t|
+    t.string 'address'
+    t.string 'address_line_two'
+    t.integer 'number'
+    t.string 'district'
+    t.string 'city'
+    t.string 'state'
+    t.string 'zip_code'
+    t.boolean 'main_address'
+    t.integer 'customer_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['customer_id'], name: 'index_addresses_on_customer_id', unique: true
   end
 
-  create_table "cars", force: :cascade do |t|
-    t.string "model"
-    t.string "brand"
-    t.string "color"
-    t.integer "year"
-    t.integer "customer_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_cars_on_customer_id", unique: true
+  create_table 'cars', force: :cascade do |t|
+    t.string 'model'
+    t.string 'brand'
+    t.string 'color'
+    t.integer 'year'
+    t.integer 'customer_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['customer_id'], name: 'index_cars_on_customer_id', unique: true
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.bigint "social_security_number"
-    t.date "birth_date", null: false
-    t.date "last_purchase_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_customers_on_email", unique: true
+  create_table 'customers', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'email', null: false
+    t.bigint 'social_security_number'
+    t.date 'birth_date', null: false
+    t.date 'last_purchase_date'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_customers_on_email', unique: true
   end
 
-  create_table "order_products", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "product_id", null: false
-    t.decimal "total", precision: 20, scale: 4, default: "0.0", null: false
-    t.integer "quantity", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_order_products_on_order_id"
-    t.index ["product_id"], name: "index_order_products_on_product_id"
+  create_table 'order_products', force: :cascade do |t|
+    t.integer 'order_id', null: false
+    t.integer 'product_id', null: false
+    t.decimal 'total', precision: 20, scale: 4, default: '0.0', null: false
+    t.integer 'quantity', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['order_id'], name: 'index_order_products_on_order_id'
+    t.index ['product_id'], name: 'index_order_products_on_product_id'
   end
 
-  create_table "order_services", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "service_id", null: false
-    t.integer "service_duration", null: false
-    t.datetime "scheduled_time", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_order_services_on_order_id"
-    t.index ["service_id"], name: "index_order_services_on_service_id"
+  create_table 'order_services', force: :cascade do |t|
+    t.integer 'order_id', null: false
+    t.integer 'service_id', null: false
+    t.integer 'service_duration', null: false
+    t.datetime 'scheduled_time', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['order_id'], name: 'index_order_services_on_order_id'
+    t.index ['service_id'], name: 'index_order_services_on_service_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.decimal "total", default: "0.0", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  create_table 'orders', force: :cascade do |t|
+    t.integer 'customer_id', null: false
+    t.decimal 'total', default: '0.0', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['customer_id'], name: 'index_orders_on_customer_id'
   end
 
-  create_table "phones", force: :cascade do |t|
-    t.string "country_code"
-    t.string "local_code"
-    t.bigint "number"
-    t.integer "customer_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_phones_on_customer_id", unique: true
+  create_table 'phones', force: :cascade do |t|
+    t.string 'country_code'
+    t.string 'local_code'
+    t.bigint 'number'
+    t.integer 'customer_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['customer_id'], name: 'index_phones_on_customer_id', unique: true
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.decimal "price"
-    t.integer "quantity"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'products', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.decimal 'price'
+    t.integer 'quantity'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "services", force: :cascade do |t|
-    t.string "title"
-    t.decimal "price"
-    t.text "description"
-    t.datetime "scheduled_time"
-    t.integer "duration"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'services', force: :cascade do |t|
+    t.string 'title'
+    t.decimal 'price'
+    t.text 'description'
+    t.datetime 'scheduled_time'
+    t.integer 'duration'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "nickname"
-    t.string "email"
-    t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["nickname"], name: "index_users_on_nickname", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'nickname'
+    t.string 'email'
+    t.string 'password_digest', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['nickname'], name: 'index_users_on_nickname', unique: true
   end
 
-  add_foreign_key "addresses", "customers"
-  add_foreign_key "cars", "customers"
-  add_foreign_key "order_products", "orders"
-  add_foreign_key "order_products", "products"
-  add_foreign_key "order_services", "orders"
-  add_foreign_key "order_services", "services"
-  add_foreign_key "orders", "customers"
-  add_foreign_key "phones", "customers"
+  add_foreign_key 'addresses', 'customers'
+  add_foreign_key 'cars', 'customers'
+  add_foreign_key 'order_products', 'orders'
+  add_foreign_key 'order_products', 'products'
+  add_foreign_key 'order_services', 'orders'
+  add_foreign_key 'order_services', 'services'
+  add_foreign_key 'orders', 'customers'
+  add_foreign_key 'phones', 'customers'
 end
